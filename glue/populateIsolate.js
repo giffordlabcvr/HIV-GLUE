77c3bff2352eccdb7458002fe0f3f9150737a167
Rob Gifford <robjgiff@gmail.com>
2017-10-16 13:47:42 +0100

    Final updates to project build to bring in Hunter transmission pair data

--- glue/populateIsolate.js
+++ glue/populateIsolate.js
@@ -1,6 +1,26 @@
+// Load data from Iyer study
 var loadResult;
 glue.inMode("module/hivTabularUtility", function() {
 	loadResult = glue.command(["load-tabular", "tabular/transmission-pairs/iyer/iyer_isolate.txt"], 
+			{convertTableToObjects:true});
+	// glue.log("INFO", "load result was:", loadResult);
+});
+
+_.each(loadResult, function(isolateObj) {
+	glue.command(["create", "custom-table-row", "dr_isolate", isolateObj.id]);
+
+	glue.inMode("custom-table-row/dr_isolate/"+isolateObj.id, function() {
+	
+		glue.command(["set", "field", "patient_id", isolateObj.patient_id]);
+		glue.command(["set", "field", "sample_date", isolateObj.sample_date]);
+		glue.command(["set", "field", "seq_type", isolateObj.seq_type]);
+
+	});
+});
+
+// Load data from Hunter study
+glue.inMode("module/hivTabularUtility", function() {
+	loadResult = glue.command(["load-tabular", "tabular/transmission-pairs/hunter/hunter_isolate.txt"], 
 			{convertTableToObjects:true});
 	// glue.log("INFO", "load result was:", loadResult);
 });
