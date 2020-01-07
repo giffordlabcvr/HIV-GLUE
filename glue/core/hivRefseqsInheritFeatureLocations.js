9f81414b83c63d14bfa206712885d77a9f480c8b
Rob Gifford <robjgiff@gmail.com>
2020-01-07 23:44:04 +0000

    Validated - Inherit explicitly defined sets of features from MASTER to other reference sequences

--- glue/core/hivRefseqsInheritFeatureLocations.js
+++ glue/core/hivRefseqsInheritFeatureLocations.js
@@ -1,27 +1,31 @@
+//list of parent features which should be transferred - the parent of the list elements is 'whole_genome'
+var nonCodingFeaturesToInherit = ["PBS", "TAR", "PSI"];
+
+
 //initially remove all feature location annotations from the reference sequences with the exception of the master
-glue.command(["multi-delete", "feature_location", "-w", "referenceSequence.name != 'REF_MASTER_B_AF033819'"]);
+//glue.command(["multi-delete", "feature_location", "-w", "referenceSequence.name != 'REF_MASTER_B_AF033819'"]);
 
 //list all HIV-1 reference sequences
-var refSeqObjs = glue.tableToObjects(glue.command(["list", "reference", "name", "-w", "sequence.species='HIV-1'"]));
+var refSeqObjs = glue.tableToObjects(glue.command(["list", "reference", "name", "-w", "sequence.species_group='M' "]));
 
-
-//list of parent features which should be transferred - the parent of the list elements is 'whole_genome'
-var codingFeaturesToCheck = ["MA", "CA", "NC"];
 
 
 _.each(refSeqObjs, function(refSeqObj) {
 
 	if (refSeqObj.name != 'REF_MASTER_B_AF033819') {
 
-		glue.inMode("reference/"+refSeqObj.name, function() {
-			glue.command(["inherit", "feature-location", 
-				"--recursive", "--spanGaps", 
-				"AL_REF_HIV-1_UNCONSTRAINED", "--relRefName", "REF_MASTER_B_AF033819", "p6"]);
-		});
+		for(var k = 0; k < nonCodingFeaturesToInherit.length; k++) {
+			var featureID = nonCodingFeaturesToInherit[k];
+			glue.logInfo(" Processing feature: "+featureID+"in reference "+refSeqObj.name);		
+
+			glue.inMode("reference/"+refSeqObj.name, function() {
+				glue.command(["inherit", "feature-location", 			
+					"AL_REF_HIV-1_UNCONSTRAINED", "-l", "REF_MASTER_B_AF033819", featureID]);
+			});
+			
+		}
 	}
 
 });
 
 
-//perform checks to ensure feature inheritance worked correctly
-// check aa translation of coding features for 'M' & '*'
