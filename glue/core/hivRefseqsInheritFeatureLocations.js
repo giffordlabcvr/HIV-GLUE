b2d05e1a3baf9a437923fd8e15b5313588ef82c4
Samantha Campbell <samanthacampbell360@gmail.com>
2020-03-10 10:51:09 +0000

    added checks for some coding regions - produces warnings but doesn't exit script

--- glue/core/hivRefseqsInheritFeatureLocations.js
+++ glue/core/hivRefseqsInheritFeatureLocations.js
@@ -1,18 +1,18 @@
 var refSequence = "REF_MASTER_B_AF033819";
 
-// list of coding features to exclude
 //var nonCoding = ["5LTR", "5R", "5U5", "TAR", "PBS", "DIS", "SD", "SL3", "PSI", "SL4", "5UTR", "TSS", "cPPT", "CTS", "RRE", "PPT", "3U3", "3R"];
 
+// get all features annotated on the master reference
 var allFeatures = [];
 glue.inMode("reference/"+refSequence, function() {
 
 	allFeatures = glue.tableToObjects(glue.command(["list", "feature-location"]));
 });
 
+// coding features to check when inheriting annotations 
 var featuresToCheck = ["gag", "gag-pol", "tat", "vpr", "vif", "vpu", "nef"];
 
 
-//glue.logInfo("allFeatures['feature.name']", allFeatures['feature.name']);
 //list all HIV-1 reference sequences
 var refSeqObjs = glue.tableToObjects(glue.command(["list", "reference", "name", "-w", "sequence.species_group='M' "]));
 
@@ -29,18 +29,23 @@ _.each(refSeqObjs, function(refSeqObj) {
 			glue.inMode("reference/"+ refSeqObj.name, function(){
 				glue.command(["inherit", "feature-location", "AL_ROOT_UNCONSTRAINED", "-l", refSequence, f]);
 					
-				if (_.has(featuresToCheck, f)){
+				if (_.contains(featuresToCheck, f)){
 					glue.logInfo("Checking feature:   " +f);
 					glue.inMode("feature-location/"+f, function() {
 						var aaRows = glue.tableToObjects(glue.command(["amino-acid"]));
 						for(var i = 0; i < aaRows.length; i++) {
 							var aa = aaRows[i].aminoAcid;
+							// check coding region begins with start codon
 							if(i == 0 && aa != "M") {
 								glue.log("WARNING", "Residue "+aaRows[i].codonLabel+" of feature "+f+" on reference "+refSeqObj.name+" should be M");
-							
-							
-							// add check for stop codon
-							
+							}
+							// check for premature stop codons
+							if(i < aaRows.length-1 && aa == "*") {
+								glue.log("WARNING", "Residue "+aaRows[i].codonLabel+" of feature "+f+" on reference "+refSeqObj.name+" should not be *");
+							}
+							// check that last codon is a stop codon
+							if(i == aaRows.length-1 && aa != "*") {
+								glue.log("WARNING", "Residue "+aaRows[i].codonLabel+" of feature "+f+" on reference "+refSeqObj.name+" should be *");
 							}
 						}
 						
