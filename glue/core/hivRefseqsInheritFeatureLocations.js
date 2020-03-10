e1d84e09b32916d3689c1882c4fa8b05ae98587b
Samantha Campbell <samanthacampbell360@gmail.com>
2020-03-10 11:49:29 +0000

    added statement to exclude 'whole_genome' feature

--- glue/core/hivRefseqsInheritFeatureLocations.js
+++ glue/core/hivRefseqsInheritFeatureLocations.js
@@ -25,34 +25,37 @@ _.each(refSeqObjs, function(refSeqObj) {
 		_.each(_.pairs(allFeatures), function(pair) {	
 			var f = pair[1]['feature.name'];
 			
-			glue.logInfo("Inheriting feature: "+f+" from REF_MASTER to "+refSeqObj.name);
-			glue.inMode("reference/"+ refSeqObj.name, function(){
-				glue.command(["inherit", "feature-location", "AL_ROOT_UNCONSTRAINED", "-l", refSequence, f]);
-					
-				if (_.contains(featuresToCheck, f)){
-					glue.logInfo("Checking feature:   " +f);
-					glue.inMode("feature-location/"+f, function() {
-						var aaRows = glue.tableToObjects(glue.command(["amino-acid"]));
-						for(var i = 0; i < aaRows.length; i++) {
-							var aa = aaRows[i].aminoAcid;
-							// check coding region begins with start codon
-							if(i == 0 && aa != "M") {
-								glue.log("WARNING", "Residue "+aaRows[i].codonLabel+" of feature "+f+" on reference "+refSeqObj.name+" should be M");
-							}
-							// check for premature stop codons
-							if(i < aaRows.length-1 && aa == "*") {
-								glue.log("WARNING", "Residue "+aaRows[i].codonLabel+" of feature "+f+" on reference "+refSeqObj.name+" should not be *");
-							}
-							// check that last codon is a stop codon
-							if(i == aaRows.length-1 && aa != "*") {
-								glue.log("WARNING", "Residue "+aaRows[i].codonLabel+" of feature "+f+" on reference "+refSeqObj.name+" should be *");
-							}
-						}
+			
+			if (f != 'whole_genome') {
+				glue.logInfo("Inheriting feature: "+f+" from REF_MASTER to "+refSeqObj.name);
+				glue.inMode("reference/"+ refSeqObj.name, function(){
+					glue.command(["inherit", "feature-location", "AL_ROOT_UNCONSTRAINED", "-l", refSequence, f]);
 						
-					});
-				}
-		
-			});
+					if (_.contains(featuresToCheck, f)){
+						glue.logInfo("Checking feature:   " +f);
+						glue.inMode("feature-location/"+f, function() {
+							var aaRows = glue.tableToObjects(glue.command(["amino-acid"]));
+							for(var i = 0; i < aaRows.length; i++) {
+								var aa = aaRows[i].aminoAcid;
+								// check coding region begins with start codon
+								if(i == 0 && aa != "M") {
+									glue.log("WARNING", "Residue "+aaRows[i].codonLabel+" of feature "+f+" on reference "+refSeqObj.name+" should be M");
+								}
+								// check for premature stop codons
+								if(i < aaRows.length-1 && aa == "*") {
+									glue.log("WARNING", "Residue "+aaRows[i].codonLabel+" of feature "+f+" on reference "+refSeqObj.name+" should not be *");
+								}
+								// check that last codon is a stop codon
+								if(i == aaRows.length-1 && aa != "*") {
+									glue.log("WARNING", "Residue "+aaRows[i].codonLabel+" of feature "+f+" on reference "+refSeqObj.name+" should be *");
+								}
+							}
+							
+						});
+					}
+			
+				});
+			}	
 			
 		});
 	}						
